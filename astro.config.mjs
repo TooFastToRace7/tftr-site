25bdaceb9c7a0a78a91a27e6eb5c7584ed5dd576
TooFastToRace <info@srogersengineering.co.uk>
2026-05-13 13:50:54 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,13 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://toofasttorace.com',
   integrations: [sitemap()],
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      cssCodeSplit: true,
+      assetsInlineLimit: 4096,
+    },
+  },
 });
